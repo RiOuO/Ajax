6c93ed693501a12476b2b17a245d0e321a233eca
RiOuO <128416852+RiOuO@users.noreply.github.com>
2023-07-13 22:40:40 +0800

    xx

---
+++ MS147Site/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MS147Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Hi");
+            //return Content("<H1>Hi</H1>","text/html");
+            return Content("<H1>你好挖</H1>", "text/html",System.Text.Encoding.UTF8);
+        }
+    }
+}
---
+++ MS147Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MS147Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
